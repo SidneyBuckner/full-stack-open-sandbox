f0ece5288c5e1508fa250296728923a734993b04
Sidney Buckner <sidney@murmur.com>
2022-04-11 15:34:34 -0400

    Part 1 Chapter 1 completion or exercise examples.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
-const App = ()=>(<div>
-  <p>Hello World!</p>
-</div>)
+// const App = () => {
+//   const now = new Date()
+//   const a = 10;
+//   const b = 20;
 
+//   return (
+//     <div>
+//       <p>Hello world, it is {now.toString()}</p>
+//       <p>
+//         {a} plus {b} is {a + b}
+//       </p>
+//     </div>
+//   )
+// }
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello, {props.name}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Sidney"/>
+    </div>
+  )
+}
 export default App;
