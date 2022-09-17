c75d6d532b410d12c07e468bcb5ff04117d5e69d
Sidney Buckner <sidney@murmur.com>
2022-09-17 15:37:47 -0400

    Actually commiting changes!

--- exercise-course-info/src/App.js
+++ exercise-course-info/src/App.js
@@ -1,50 +1,59 @@
 const App = () => {
-  const course = 'Half Stack Application Development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = "Half Stack application development";
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14
+  };
 
-  const Header = (props)=>{
-    console.log(props);
-    return(
+  const Header = () => {
+    return (
       <div>
-        <h1>{props.course}</h1>
+        <h1>{course}</h1>
       </div>
-    )
-    }
-    
-    const Total = ()=>{ 
-      return(
+    );
+  };
+
+  const Total = () => {
+    return (
       <div>
-        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-        
-      </div>)}
-    
-    const Content = ()=>{
-    return(
-      <div>
-         <p>
-            {part1} {exercises1}
-          </p>
-          <p>
-            {part2} {exercises2}
-          </p>
-          <p>
-            {part3} {exercises3}
-          </p>
+        <p>
+          Number of exercises:{" "}
+          {part1.exercises + part2.exercises + part3.exercises}
+        </p>
       </div>
-    )
-    }
+    );
+  };
+
+  const Content = () => {
+    return (
+      <div>
+        <p>
+          {part1.name}: {part1.exercises}
+        </p>
+        <p>
+          {part2.name}: {part2.exercises}
+        </p>
+        <p>
+          {part3.name}: {part3.exercises}
+        </p>
+      </div>
+    );
+  };
   return (
     <div>
       <Header course={course} />
       <Content />
       <Total />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
