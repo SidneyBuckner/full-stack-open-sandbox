58abe8a9565d6c07a497bb17f98044606cdf3f62
Sidney Buckner <sidney.buckner2@gmail.com>
2022-09-26 10:45:18 -0400

    1.3 Solution

--- exercise-course-info/src/App.js
+++ exercise-course-info/src/App.js
@@ -9,59 +9,75 @@ const Header = (props)=>{
   //The Part component takes in the parameter props as an object
   // and tells us how the props will be displayed
   // placeholders here!!!!!
-const Part = (props)=>{
-  return (<p>
-    {props.part} {props.exercise}
-  </p>);
-}
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  );
+};
 
 //The Content component gives us corresponding fields
 // of the Props in Part component and defines what data will explicitly be displayed
-const Content = (props)=>{
-  return(
-  <div>
-    <Part part={props.part1} exercise={props.exercises1}/>
-    <Part part={props.part2} exercise={props.exercises2}/>
-    <Part part={props.part3} exercise={props.exercises3}/>
-  </div>
-  )
+const Content = (props) => {
+  return (
+    <div>
+      {/* Were calling name to indicate props.name1 as the direct 
+      data we intend to display from what we return in App */}
+      <Part name={props.name1} exercises={props.exercises1} />
+      <Part name={props.name2} exercises={props.exercises2} />
+      <Part name={props.name3} exercises={props.exercises3} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises:{" "}
+      {props.exercises1 + props.exercises2 + props.exercises3}
+    </p>
+  );
+};
+
+const App = () =>
+{
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
   }
 
-  const Total = (props)=>{ 
-    return(
-    <div>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p> 
-    </div>)}
-
-const App = () => {
-  const course = 'Half Stack Application Development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-
+  //HERE is where we define the object's actual names and placeholders for the data we intend to fill in the props above! 
+  // name1 is what we indicate as part1.name that we want to return in content 
   return (
     <div>
       <Header course={course} />
-      <Content        
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3} 
-        />
-      <Total         
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
 
-        />
+      <Content
+        name1={part1.name}
+        name2={part2.name}
+        name3={part3.name}
+        exercises1={part1.exercises}
+        exercises2={part2.exercises}
+        exercises3={part3.exercises}
+      />
+
+      <Total
+        exercises1={part1.exercises}
+        exercises2={part2.exercises}
+        exercises3={part3.exercises}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default App;
