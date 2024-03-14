c71fc4daef8315ccaff3236d0ce4dead0f70d3df
Sidney Buckner <sidney.buckner2@gmail.com>
2024-03-14 14:27:22 -0400

    Refactor Code to where we keep up with ONE set of state instead of three

--- 1-unicafe/src/App.jsx
+++ 1-unicafe/src/App.jsx
@@ -3,42 +3,31 @@ import { useState } from 'react'
 import './App.css'
 
 const App = ()=> {
-  // State variables to track the number of clicks for each feedback category
-  const [goodCount, setGoodCount] = useState(0);
-  const [neutralCount, setNeutralCount] = useState(0);
-  const [badCount, setBadCount] = useState(0);
+const [feedbackCount, setFeedbackCount] = useState({good: 0,
+neutral: 0, bad: 0})
 
   // Function to handle feedback submission for each category
-  const handleGoodFeedback = () => {
-    setGoodCount((prevCount) => prevCount + 1);
+  const handleFeedback = (feedbackType) => {
+    setFeedbackCount((prevCount) => ({ ...prevCount, [feedbackType]: prevCount[feedbackType] + 1}));
   };
-
-  const handleNeutralFeedback = () => {
-    setNeutralCount((prevCount) => prevCount + 1);
-  };
-
-  const handleBadFeedback = () => {
-    setBadCount((prevCount) => prevCount + 1);
-  };
-
   return (
     <div>
       <h1>Unicafe Customer Feedback</h1>
       <p>How is our service? Please choose from the options below:</p>
       <div>
-        <button onClick={handleGoodFeedback}>Good</button>
-        <button onClick={handleNeutralFeedback}>Neutral</button>
-        <button onClick={handleBadFeedback}>Bad</button>
+        <button onClick={()=> handleFeedback('good')}>Good</button>
+        <button onClick={()=> handleFeedback('neutral')}>Neutral</button>
+        <button onClick={()=> handleFeedback('bad')}>Bad</button>
       </div>
       <div className='card'>
         <h2>Feedback Summary</h2>
-        <p>Good: {goodCount}</p>
-        <p>Neutral: {neutralCount}</p>
-        <p>Bad: {badCount}</p>
+        <p>Good: {feedbackCount.good}</p>
+        <p>Neutral: {feedbackCount.neutral}</p>
+        <p>Bad: {feedbackCount.bad}</p>
       </div>
     </div>
   );
 }
 
 
-export default App
+export default App;
