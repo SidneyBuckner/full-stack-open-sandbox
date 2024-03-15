d8288f583360336c794b8309b563444752a49af9
Sidney Buckner <sidney.buckner2@gmail.com>
2024-03-15 11:21:27 -0400

    Exercise 1.8 :
    Refactor your application so that displaying the statistics is extracted into its own Statistics component. The state of the application should remain in the App root component.
    
    Exercise 1.9:
    
    Change your application to display statistics only once feedback has been gathered.

--- 1-unicafe/src/App.jsx
+++ 1-unicafe/src/App.jsx
@@ -2,14 +2,8 @@ import { useState } from 'react'
 
 import './App.css'
 
-const App = ()=> {
-const [feedbackCount, setFeedbackCount] = useState({good: 0,
-neutral: 0, bad: 0})
-const feedbackTotal = feedbackCount.good + feedbackCount.bad + feedbackCount.neutral;
-  // Function to handle feedback submission for each category
-  const handleFeedback = (feedbackType) => {
-    setFeedbackCount((prevCount) => ({ ...prevCount, [feedbackType]: prevCount[feedbackType] + 1}));
-  };
+const Statistics = ({feedbackCount})=>{
+  const feedbackTotal = feedbackCount.good + feedbackCount.bad + feedbackCount.neutral;
   const calculateAverage = (goodFeedback, badFeedback, neutralFeedback, totalResponses)=>{
     const goodFeebackPoints = goodFeedback * 1;
     const badFeedbackPoints = badFeedback * 0
@@ -18,6 +12,34 @@ const feedbackTotal = feedbackCount.good + feedbackCount.bad + feedbackCount.neu
     return (goodFeebackPoints + badFeedbackPoints + neutralFeedbackPoints)/totalResponses;
 
   }
+  if(feedbackTotal === 0){
+    return <div>
+      No Feedback Given
+    </div>
+  }
+  return( 
+  <div className='card'>
+    <h2>Statistics</h2>
+    <p>Good: {feedbackCount.good}</p>
+    <p>Neutral: {feedbackCount.neutral}</p>
+    <p>Bad: {feedbackCount.bad}</p>
+    <p>Total: {feedbackTotal}</p>
+          {/* average is caluated with multiplying the amount of good neutral and bad by 1 0 or negative 1 and diving that number by the total amount of answers*/}
+    <p>Average: {!feedbackTotal ? 0: (calculateAverage(feedbackCount.good,feedbackCount.bad,feedbackCount.neutral,feedbackTotal ))}</p>
+    <p>Positive: { !feedbackTotal ? 0:feedbackCount.good / (feedbackCount.good + feedbackCount.bad + feedbackCount.neutral)*100}%</p>
+    </div>)
+
+
+}
+
+
+const App = ()=> {
+const [feedbackCount, setFeedbackCount] = useState({good: 0,
+neutral: 0, bad: 0})
+  // Function to handle feedback submission for each category
+  const handleFeedback = (feedbackType) => {
+    setFeedbackCount((prevCount) => ({ ...prevCount, [feedbackType]: prevCount[feedbackType] + 1}));
+  };
 
   return (
     <div>
@@ -28,17 +50,7 @@ const feedbackTotal = feedbackCount.good + feedbackCount.bad + feedbackCount.neu
         <button onClick={()=> handleFeedback('neutral')}>Neutral</button>
         <button onClick={()=> handleFeedback('bad')}>Bad</button>
       </div>
-      <div className='card'>
-        <h2>Statistics</h2>
-        <p>Good: {feedbackCount.good}</p>
-        <p>Neutral: {feedbackCount.neutral}</p>
-        <p>Bad: {feedbackCount.bad}</p>
-        <p>Total: {feedbackTotal}</p>
-        {/* average is caluated with multiplying the amount of good neutral and bad by 1 0 or negative 1 and diving that number by the total amount of answers*/}
-
-        <p>Average: {!feedbackTotal ? 0: (calculateAverage(feedbackCount.good,feedbackCount.bad,feedbackCount.neutral,feedbackTotal ))}</p>
-        <p>Positive: { !feedbackTotal ? 0: feedbackCount.good / (feedbackCount.good + feedbackCount.bad + feedbackCount.neutral)*100}%</p>
-      </div>
+      <Statistics feedbackCount={feedbackCount}/>
     </div>
   );
 }
