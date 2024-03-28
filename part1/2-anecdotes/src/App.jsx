7cb83a6fa3b4879d7601dddd3a5f758c5e27dbfb
Sidney Buckner <sidney.buckner2@gmail.com>
2024-03-28 13:31:05 -0400

    1.14*: anecdotes step3
    Now implement the final version of the application that displays the anecdote with the largest number of votes. Add a reset button to make all vote counts for anecdotes to 0.

--- part1/2-anecdotes/src/App.jsx
+++ part1/2-anecdotes/src/App.jsx
@@ -2,11 +2,6 @@ import { useState } from 'react'
 
 const App = () => {
   const [selected, setSelected] = useState(0);
-  const handleClick = ()=>{
-  const selected = Math.floor(Math.random()*11);
-return setSelected(selected);
-
-  }
   const anecdotes = [
     'If it hurts, do it more often.',
     'When we hit our lowest point, we are open to the greatest change.',
@@ -19,15 +14,36 @@ return setSelected(selected);
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
-  ]
-// Get random number when clicking button
+  ];
 
+  const points = new Array(anecdotes.length).fill(0);
+  const [pointCount, setPointCount] = useState(points);
+  const mostPopularAnecdote = pointCount.indexOf(Math.max(...pointCount));
+// Get random number when clicking button
+const handleClick = ()=>{
+  const selected = Math.floor(Math.random()*11);
+  return setSelected(selected);
+  }
+  const handleVote = () => {
+    const pointsCopy = [...pointCount];
+    pointsCopy[selected]+=1;
+    setPointCount(pointsCopy);
+  }
+  const handleVoteReset = ()=>{
+    setPointCount(points);
+  }
   return (
     <div>
-      {anecdotes[selected]}
+      <p>{anecdotes[selected]}</p>
+      <p> This has {pointCount[selected]} Votes</p>
       <button onClick={()=>{handleClick()}}>next anecdote: </button>
+      <button onClick={()=>{handleVote()}}>Vote for this anecdote!</button>
+      <button onClick={()=>{handleVoteReset()}}>Reset All Votes</button>
+
+      <h3>Anecdote with the most votes: </h3>
+      <p>"{anecdotes[mostPopularAnecdote]}" has: {pointCount[mostPopularAnecdote]}  points</p>
     </div>
   )
 }
 
-export default App
+export default App;
