784ee91251cf534c529a1e42542514cece05d42c
Sidney Buckner <sidney.buckner2@gmail.com>
2024-04-05 12:42:44 -0400

    Collections should be mapped to a specific place so that their values are not hard coded and repeated. This is to Keep code DRY and readable as much as possible.

--- part2/1-redneringCollections/src/App.jsx
+++ part2/1-redneringCollections/src/App.jsx
@@ -5,9 +5,9 @@ const App = (props) => {
     <div>
       <h1>Notes</h1>
       <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
+        {notes.map((note)=>
+          <li>{note.content}</li>
+        )}
       </ul>
     </div>
   )
